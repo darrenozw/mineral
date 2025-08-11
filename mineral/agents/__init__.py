8520da86da59421ae9798ca4351dd6333715000d
Zheng Wei Ong <uujsd@student.kit.edu>
2025-08-11 10:20:06 +0200

    fork and push to mineral

--- mineral/agents/__init__.py
+++ mineral/agents/__init__.py
@@ -4,3 +4,7 @@ from .diffrl.bptt import BPTT
 from .diffrl.shac import SHAC
 from .ppo.ppo import PPO
 from .sac.sac import SAC
+
+# Add new agent here
+# from .random.random import RandomAgent
+from .cemmpc.cemmpc import CEMMPCAgent
